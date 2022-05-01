f9f625a3690f3a663ba5c80ea52bff66a1fd1cf0
farley0909 <80477605+farley0909@users.noreply.github.com>
2022-05-01 17:08:44 -0300

    Adicionando o código da questão 3

---
+++ Ques3.java
@@ -0,0 +1,6 @@
+//crie um programa Java que imprima o seguinte texto “Terminei a primeira aula com um programa Java!".
+public class Ques3{
+    public static void main(String[] args) {
+        System.out.print("Terminei a primeira aula com um programa Java!");
+    }
+}
